eb4939f022e8a32c7b390914ad9f67604964b0c1
sreyas <sreyaskjayan22@gmail.com>
2025-01-11 14:46:11 +0530

    name api

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/:id/', function (req, res) {
+  const id  = req.params.id
+  if(id=="1"){
+    return res.send('Sreyas\n');
+  }
+  return res.send('Samarth\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
